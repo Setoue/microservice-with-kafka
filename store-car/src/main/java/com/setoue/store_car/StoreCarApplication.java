433d08b756f480137a09c6665c6ac9057f3d2936
Setoue <proshigeru@gmail.com>
2025-07-28 20:47:20 -0300

    feat: add service store-car

---
+++ store-car/src/main/java/com/setoue/store_car/StoreCarApplication.java
@@ -0,0 +1,13 @@
+package com.setoue.store_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCarApplication.class, args);
+	}
+
+}
