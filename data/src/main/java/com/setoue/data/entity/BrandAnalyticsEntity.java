26fe802b6188091868f1e10512fcaec59a45689d
Setoue <proshigeru@gmail.com>
2025-08-19 17:48:48 -0300

    feat: add entities

--- data/src/main/java/com/setoue/data/entity/BrandAnalyticsEntity.java
+++ data/src/main/java/com/setoue/data/entity/BrandAnalyticsEntity.java
@@ -1,4 +1,23 @@
 package com.setoue.data.entity;
 
-public class BrandAnalyticsEntity {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "brand_analytics")
+@Data
+@NoArgsConstructor
+public class BrandAnalyticsEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String brand;
+
+    private Long posts;
+
 }
--- data/src/main/java/com/setoue/data/entity/CarModelPriceEntity.java
+++ data/src/main/java/com/setoue/data/entity/CarModelPriceEntity.java
@@ -1,4 +1,20 @@
 package com.setoue.data.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "car_model_price")
+@Data
+@NoArgsConstructor
 public class CarModelPriceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String model;
+
+    private Double price;
 }
